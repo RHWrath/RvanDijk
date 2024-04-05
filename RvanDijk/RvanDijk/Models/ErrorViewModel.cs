41f8d4f588b1bccda73ccad5bbe88000836f3818
RHWrath <Rik.Heerholtz@gmail.com>
2024-04-05 09:19:44 +0200

    first publish

---
+++ RvanDijk/RvanDijk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RvanDijk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
